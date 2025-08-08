90821655eef042655366646ea9c2866da8081dd5
void-57 <ruddhaani60@gmail.com>
2025-08-08 18:51:57 +0530

    refactor: Revamp notification system with improved styling and functionality

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -222,19 +222,26 @@ document.addEventListener("popupclosed", (e) => {
 
 //Function for displaying toast notifications.
 function notify(message, mode, options = {}) {
-  let icon;
-  switch (mode) {
-    case "success":
-      icon = `<svg class="icon icon--success" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"/></svg>`;
-      break;
-    case "error":
-      icon = `<svg class="icon icon--error" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-7v2h2v-2h-2zm0-8v6h2V7h-2z"/></svg>`;
-      break;
-  }
-  if (mode === "error") {
-    console.error(message);
-  }
-  return getRef("notification_drawer").push(message, { icon, ...options });
+  const icon =
+    mode === "success"
+      ? `<svg class="icon icon--success" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"/></svg>`
+      : mode === "error"
+      ? `<svg class="icon icon--error" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-7v2h2v-2h-2zm0-8v6h2V7h-2z"/></svg>`
+      : "";
+
+  const drawer = getRef("notification_drawer");
+  if (!drawer) return;
+
+  const notification = document.createElement("div");
+  notification.className = `notification ${mode}`;
+  notification.innerHTML = `${icon}<span>${message}</span>`;
+
+  drawer.appendChild(notification);
+
+  // Auto-remove after 3 seconds
+  setTimeout(() => {
+    notification.remove();
+  }, options.duration || 3000);
 }
 
 // Input field control functions
